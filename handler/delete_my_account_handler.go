9505353bfc3eb726a89a2e2099b53d41e622d8fe
Jerry <calljerryli@outlook.com>
2022-01-22 14:59:26 +0800

    注销账号接口

---
+++ handler/delete_my_account_handler.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jerryshell/my-flomo-server/db"
+	"github.com/jerryshell/my-flomo-server/model"
+	"github.com/jerryshell/my-flomo-server/result"
+	"net/http"
+)
+
+func DeleteMyAccount(c *gin.Context) {
+	user := c.MustGet("user").(*model.User)
+	// remove memo
+	db.DB.Unscoped().Delete(model.Memo{}, "user_id = ?", user.ID)
+	// remove pluginToken
+	db.DB.Unscoped().Delete(model.PluginToken{}, "user_id = ?", user.ID)
+	// remove user
+	db.DB.Unscoped().Delete(model.User{}, "id = ?", user.ID)
+	c.JSON(http.StatusOK, result.SuccessWithDataAndMessage(nil, "此账户数据已被毫无痕迹的永久抹除！👋"))
+}
