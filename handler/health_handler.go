0292807942be0ba96cbdd734333e2da998082aea
jerry <calljerryli@outlook.com>
2024-07-23 20:49:20 +0800

    add git commit hash to /health api

--- handler/health_handler.go
+++ handler/health_handler.go
@@ -1,15 +1,30 @@
 package handler
 
 import (
+	"net/http"
+	"runtime/debug"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jerryshell/my-flomo-server/result"
-	"net/http"
 )
+
+var Commit = func() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				return setting.Value
+			}
+		}
+	}
+
+	return ""
+}()
 
 func Health(c *gin.Context) {
 	c.JSON(http.StatusOK, result.BaseResult{
 		Code:    http.StatusOK,
 		Success: true,
 		Message: "Server Online",
+		Data:    gin.H{"commit": Commit},
 	})
 }
